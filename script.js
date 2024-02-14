2b50af4eed2801f743c76fa9edf27bbee4525bd8
Anash-K <157481353+Anash-K@users.noreply.github.com>
2024-02-14 21:31:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
-
 const APIkey = "23513af0381811c48b12849bdf5087a8";
 const APIurl = "https://api.openweathermap.org/data/2.5/weather?&q=";
 
 const city = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
 
+// Default city
+const defaultCity = "Ahmedabad"; // Change this to the desired default city
 
-// let cityname = document.querySelector('.city');
-
+// Function to update weather information
 async function weatherinfo(city) {
-
     const response = await fetch(APIurl + city + `&appid=${APIkey}`);
     const data = await response.json();
 
@@ -20,14 +18,16 @@ async function weatherinfo(city) {
     const tempkelvtocel = Math.round((data.main.temp) - 273.15);
 
     tempchange.innerText = tempkelvtocel + " °C";
-
     document.querySelector(".humtext").innerText = data.main.humidity + " %";
     document.querySelector(".windtext").innerText = data.wind.speed + " km/h";
     document.querySelector(".city").innerText = data.name;
     document.querySelector(".country").innerText = data.sys.country;
-
 }
 
+// Show weather information for the default city on page load
+weatherinfo(defaultCity);
+
+// Event listener for the search button
 searchbtn.addEventListener("click", () => {
     weatherinfo(city.value);
 });
